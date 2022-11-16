a72fb446b1653964baeb452b018b10c0ac359f56
Masoud <masoudider@gmail.com>
2022-11-16 11:13:00 +0330

    Finalizing Glass order log logic implementation.

--- src/main/java/ir/novinp/productionreport/api/GlassLogController.java
+++ src/main/java/ir/novinp/productionreport/api/GlassLogController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RequestMapping("/glassLog")
 public class GlassLogController {
 
-
     @Autowired
     private GlassLogMapService mapService;
 
--- src/main/java/ir/novinp/productionreport/mapServices/implementations/WindowLogMapServiceImp.java
+++ src/main/java/ir/novinp/productionreport/mapServices/implementations/WindowLogMapServiceImp.java
@@ -116,5 +116,4 @@ public class WindowLogMapServiceImp implements WindowLogMapService {
                 .orElseThrow(RuntimeException::new);
     }
 
-
 }
